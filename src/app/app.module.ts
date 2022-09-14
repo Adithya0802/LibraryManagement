795c2acbbcbca6bba8896d06c35a5661a5d4a5b6
Vigneshwaran7708 <wvignesh671@gmail.com>
2022-09-14 17:04:35 +0530

    loginpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
